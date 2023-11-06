9097b9ca304475884a1e624c6e55a61586944362
Albaito <stanfordtramaine44@gmail.com>
2023-11-05 19:22:32 -0600

    added comments, html tags

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -1,9 +1,10 @@
-''' File is the routing for the webapplication '''
+''' File is for web app routing '''
 
 from django.urls import path
 from . import views
 
 urlpatterns =[
+    # path syntax: path('[url ender]', views.[views.py function], name=[''name for url])
     path('', views.place_list, name='place_list'),
     path('visited', views.places_visited, name='places_visited'),
     path('place/<int:place_pk>/was_visited/', views.place_was_visited, name='place_was_visited')
