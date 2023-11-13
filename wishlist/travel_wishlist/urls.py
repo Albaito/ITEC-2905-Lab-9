0e9f6a4ac097a5830a34f831cea1fd70c2f54cc5
Albaito <stanfordtramaine44@gmail.com>
2023-11-12 22:16:12 -0600

    Updated up to slide 26

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -7,5 +7,7 @@ urlpatterns =[
     # path syntax: path('[url ender]', views.[views.py function], name=[''name for url])
     path('', views.place_list, name='place_list'),
     path('visited', views.places_visited, name='places_visited'),
-    path('place/<int:place_pk>/was_visited/', views.place_was_visited, name='place_was_visited')
+    path('place/<int:place_pk>/was_visited/', views.place_was_visited, name='place_was_visited'),
+    path('place/<int:place_pk>', views.place_details, name='place_details')
+
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,29 +1,37 @@
-''' handles requests to homepage '''
+''' handles requests to different pages present in urls.py '''
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import newPlaceForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def place_list(request):
 
+    '''
+        If the request is a POST request, when the add button is clocked it'll validate the input,
+        and save to DB, then redirect it the current page
+    '''
 
     if request.method == 'POST':
         form = newPlaceForm(request.POST)
-        place = form.save() 
+        place = form.save(commit=False) 
+        place.user = request.user
         if form.is_valid(): # checks if entered data meets DB constraints
             place.save()    # saves to DB
             return redirect('place_list')
         
     # if not POST, or form is invalid, renderss page with form to add new place, and list of places
-    places = Place.objects.filter(visited=False).order_by('name')   # .filter() = SQL where
+    # based upon which user is connected
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')   # .filter() = SQL where
     new_place_form = newPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places' : places, 'new_place_form' : new_place_form})
 
-
+@login_required
 def places_visited(request):
-    visited = Place.objects.filter(visited=True)
+    visited = Place.objects.filter(user=request.user).filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited' : visited })
 
-
+@login_required
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
@@ -31,3 +39,8 @@ def place_was_visited(request, place_pk):
         place.save()
     
     return redirect('place_list')
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    return render(request, 'travel_wishlist/place_detail.html', {'place' : place})
